ab29190bf0d3a1cacc21c4b1c7a46b13f7e3b384
shajeer <shajeervu111@gmail.com>
2022-04-01 13:25:15 +0530

    first commit

---
+++ project/app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import razorpay
+from django.views.decorators.csrf import csrf_exempt
+
+
+def home(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        amount = 50000
+        client = razorpay.Client(auth=("rzp_test_U68XoHD9LUi8r6", "qshoen3NYzYQa1ijYv2rwWQA"))
+        payment = client.order.create({'amount': amount, 'currency': 'INR','payment_capture': '1'})
+
+    return render(request, 'index.html')
+
+@csrf_exempt
+def success(request):
+    return render(request, "success.html")
